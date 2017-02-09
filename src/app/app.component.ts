703f2326548970abd9c1b27b702fb2b717c367e7
Mike Green <mike.green@stgconsulting.com>
2017-02-09 16:03:45 -0700

    Worked in dynamic data and map info windows.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,18 @@ export class AppComponent {
     lat: number = 40.870680;
     lng: number = -111.879241;
 
-    coordinates = [
-        { name: 'Cross of Christ', lat: 40.870680,lng: -111.879241, label: 'A' },
-        { name: 'St. John\'s', lat: 40.746684, lng: -111.877382, label: 'B' },
-        { name: 'Redeemer' , lat: 40.715840, lng: 0-111.835086, label: 'C'},
-        { name: 'Christ' , lat: 40.648597, lng: -111.884845, label: 'D' },
-        { name: 'Grace' , lat: 40.574001, lng: -111.840283, label: 'E' },
-        { name: 'Holy Trinity' , lat: 40.510202, lng: -111.937056, label: 'F' }
+    private coordinates = [
+        // { name: 'Cross of Christ', lat: 40.870680,lng: -111.879241, logo: '' },
+        { name: 'Christ' , lat: 40.648597, lng: -111.884845, logo: 'christ-lutheran.jpg' }, //geographical center
+        { name: 'St. John\'s', lat: 40.746684, lng: -111.877382, logo: 'st-john.jpg' },
+        { name: 'Redeemer' , lat: 40.715840, lng: 0-111.835086, logo: 'redeemer-lutheran.jpg' },
+        { name: 'Grace' , lat: 40.574001, lng: -111.840283, logo: 'grace-lutheran.jpg' },
+        { name: 'Holy Trinity' , lat: 40.510202, lng: -111.937056, logo: 'holy-trinity-riverton.jpg' }
     ];
+
+    centerOn = this.coordinates[0];
+
+    zoom: number = 10;
 
     onMarkerClick(marker): void {
         console.log(marker);
