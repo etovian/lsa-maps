89dfaaea58922f4b87b55bad973499fbefc0eb72
Mike Green <mike.green@stgconsulting.com>
2017-02-09 15:34:21 -0700

    Initial work.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    title: string = 'My first angular2-google-maps project';
+    openInfoWindow: true;
+    lat: number = 40.870680;
+    lng: number = -111.879241;
+
+    coordinates = [
+        { name: 'Cross of Christ', lat: 40.870680,lng: -111.879241, label: 'A' },
+        { name: 'St. John\'s', lat: 40.746684, lng: -111.877382, label: 'B' },
+        { name: 'Redeemer' , lat: 40.715840, lng: 0-111.835086, label: 'C'},
+        { name: 'Christ' , lat: 40.648597, lng: -111.884845, label: 'D' },
+        { name: 'Grace' , lat: 40.574001, lng: -111.840283, label: 'E' },
+        { name: 'Holy Trinity' , lat: 40.510202, lng: -111.937056, label: 'F' }
+    ];
+
+    onMarkerClick(marker): void {
+        console.log(marker);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
+import { AgmCoreModule } from 'angular2-google-maps/core';
+
+let apiKey = 'AIzaSyC6HNgs7X3vqerCF2wgR0v3CjPru9rA5Pc';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        AgmCoreModule.forRoot({
+            apiKey: apiKey
+        }),
+        BrowserModule,
+        CommonModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
